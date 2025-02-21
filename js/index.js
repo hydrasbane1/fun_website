d34aacd7af4647dfd27ca6262d97b1071ec518ef
hydrasbane1 <tripp.johnson4934@gmail.com>
2025-02-21 15:26:37 -0500

    unfucked the CSS somehow
    so I unfucked the CSS by abandoning the html folder and putting the files into the main folder, this wont be a problem he says knowing it will be

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var moment = require("moment");
-import * as css from "file.css";
+import * as css from "../styles/default_styles.css";
 
 console.log("hello from JavaScript");
 console.log(moment().startOf('day').fromNow());
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,17 @@ const path = require('path');
 module.exports = {
 	mode: 'development',
 	entry: {
-		script: {import: './js/index.js', filename: 'js/[name].js'},
-		style: {import: './styles/default_styles.css', filename: 'styles/[name].css'}
+		script: {import: './js/index.js', filename: '[name].js'},
+		style: {import: './styles/default_styles.css', filename: '[name].css'}
 	},
 	output: {
 		filename: '[name].js',
 		publicPath: 'dist',
-		cssFilename:'default_styles.css',
+		cssFilename:'[name].css',
 	},
 	devServer:{
 		static: {
-			directory: path.join(__dirname,'html'),
+			directory: path.join(__dirname,'/'),
 		},
 		compress: true,
 		port: 8080,
@@ -33,7 +33,7 @@ module.exports = {
 			},
 			{
 				test:/\.css$/,
-				//exclude: /node_modules/, 
+				exclude: /node_modules/, 
 				use:['style-loader','css-loader'],
 			}
 		]
